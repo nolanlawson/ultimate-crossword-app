ac1e27110c5cfa5a8a65455a3a12b66de725e359
Nolan Lawson <nolan.lawson@gmail.com>
2013-11-06 22:41:45 -0800

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('ultimateCrosswordAppApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
