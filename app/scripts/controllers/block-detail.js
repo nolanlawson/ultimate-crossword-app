33de00c5b9f325f3717a595dc46ce68aef26de6c
Nolan Lawson <nolan.lawson@gmail.com>
2013-11-10 03:21:26 -0800

    fix error with related blocks not appearing

--- app/scripts/controllers/block-detail.js
+++ app/scripts/controllers/block-detail.js
@@ -49,6 +49,8 @@ angular.module('ultimate-crossword')
                             }
 
                             block.relatedBlocks = blocksService.transformRelatedBlocks(data.rows, block._id);
+                            block.expanded = true;
+                            
                             // all done loading
                             $scope.block = block;
                             $scope.doneLoading = true;
